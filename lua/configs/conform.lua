2fb8b83dd216a7fd4d368459652e6aa923ac457f
Tin Nguyen <nguyenvb.tin03@gmail.com>
2025-01-27 16:00:03 -0500

    (conform) add cpp file formatter

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -5,19 +5,29 @@ local options = {
     html = { "prettier" },
     json = { "prettier" },
     java = { "clang-format" },
+    cpp = { "clang-format-cpp" },
   },
   formatters = {
+    ["clang-format-cpp"] = {
+      command = "clang-format",
+    },
     ["clang-format"] = {
       env = {
-        TabWidth = 4,
         UseTab = "Always",
-        IndentWidth = 4,
-        ContinuationIndentWidth = 4,
+        TabWidth = 8,
+        IndentWidth = 8,
         ColumnLimit = 10,
-
-      }
+        -- AlignArrayOfStructures = "Right",
+        -- AlignConsecutiveAssignments = "Consecutive",
+        AlignConsecutiveMacros = "Consecutive",
+        AlignAfterOpenBracket = "Align",
+        AllowAllArgumentsOnNextLine = true,
+        AllowShortCaseExpressionOnASingleLine = true,
+        BinPackArguments = false,
+        BinPackParameters = false,
+      },
     },
-  }
+  },
 
   -- format_on_save = {
   --   -- These options will be passed to conform.format()
