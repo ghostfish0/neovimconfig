a09e5d8485d660f0054e25b75972f79406adc658
Tin Nguyen <nguyenvb.tin03@gmail.com>
2025-02-01 18:16:38 -0500

    (plugins) install leetcode.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,6 +2,20 @@ local overrides = require("configs.overrides")
 return {
 	-- { "michaeljsmith/vim-indent-object", lazy = false },
 	{
+		"kawre/leetcode.nvim",
+		cmd = "Leet",
+		build = ":TSUpdate html", -- if you have `nvim-treesitter` installed
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+			-- "ibhagwan/fzf-lua",
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+		},
+		opts = {
+			-- configuration goes here
+		},
+	},
+	{
 		"lukas-reineke/indent-blankline.nvim",
 		config = function()
 			require("ibl").setup(overrides.blankline)
